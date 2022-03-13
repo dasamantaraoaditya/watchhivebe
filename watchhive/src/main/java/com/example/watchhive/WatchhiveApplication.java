f5845f690100437abf5c86afc519be0c3847a37b
Karthikeyan Ganesan <karthikeyanganesan@LT0863.local>
2022-03-13 23:24:56 +0530

    be6 Create base code for Movie hive back end #6

---
+++ watchhive/src/main/java/com/example/watchhive/WatchhiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchhiveApplication.class, args);
+	}
+
+}
---
+++ watchhive/src/test/java/com/example/watchhive/WatchhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
